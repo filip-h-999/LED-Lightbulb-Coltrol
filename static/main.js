115fe53dbdc510c96d585e9c6cba99a142444ad2
filip-h-999 <filip.hrenovcik@gmail.com>
2023-12-18 17:43:38 +0100

    Refactor button event listeners and add click sound

--- static/main.js
+++ static/main.js
@@ -1,15 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let buttons = document.querySelectorAll('.btn');
+document.addEventListener("DOMContentLoaded", function () {
+	let buttons = document.querySelectorAll(".btn");
 
-    buttons.forEach(button => {
-        button.addEventListener('mouseover', function() {
-            buttons.forEach(btn => {
-                if (btn !== this) btn.classList.add('darken');
-            });
-        });
-    
-        button.addEventListener('mouseout', function() {
-            buttons.forEach(btn => btn.classList.remove('darken'));
-        });
-    });
+	buttons.forEach((button) => {
+		if (!window.matchMedia("only screen and (max-width: 760px)").matches) {
+			button.addEventListener("mouseover", function () {
+				buttons.forEach((btn) => {
+					if (btn !== this) btn.classList.add("darken");
+				});
+			});
+
+			button.addEventListener("mouseout", function () {
+				buttons.forEach((btn) => btn.classList.remove("darken"));
+			});
+		}
+		button.addEventListener("click", function () {
+			let audio = new Audio("/static/audio/click.mp3");
+			audio.play();
+		});
+	});
 });
